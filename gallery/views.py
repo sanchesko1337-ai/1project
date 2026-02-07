6514eedf043a2cbf5b0c8cf63379483bc6ee3d43
sanchesko1337-ai <sanchesko1337@gmail.com>
2026-02-07 15:56:05 +0700

    3 save

--- gallery/views.py
+++ gallery/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-
+from .models import Asset # Импортируем модель, чтобы спрашивать данные
 def home(request):
-    # Имитация данных из базы (список словарей)
-    fake_database = [
-        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
-        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
-        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
-    ]
+    # ORM Запрос: "Дай мне все объекты Asset из базы"
+    assets = Asset.objects.all()
     context_data = {
         'page_title': 'Главная Галерея',
-        'assets': fake_database, # Передаем весь список
+        'assets': assets, # Передаем реальный QuerySet (список)
     }
     return render(request, 'gallery/index.html', context_data)
 
